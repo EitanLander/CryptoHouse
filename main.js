6b7ce570f66b22a43c40dda793a2b0bc294c71db
EitanLander <eitanlander@gmail.com>
2023-07-14 00:01:09 +0300

    Modal slider position fixed, navbar - selected coins display fixed.

--- main.js
+++ main.js
@@ -3,23 +3,23 @@
 "use strict";
 
 $(() => {
-   function scrollToTop() {
-     $("html, body").animate({ scrollTop: 0 }, "slow");
-   }
+  function scrollToTop() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  }
 
-   // Scroll to top when the "Scroll Up" link is clicked
-   $("#scrollTop").click(function () {
-     scrollToTop();
-   });
+  // Scroll to top when the "Scroll Up" link is clicked
+  $("#scrollTop").click(function () {
+    scrollToTop();
+  });
 
-   // Toggle the "Scroll Up" link visibility based on scroll position
-   $(window).scroll(function () {
-     if ($(this).scrollTop() > 100) {
-       $("#scrollTop").fadeIn();
-     } else {
-       $("#scrollTop").fadeOut();
-     }
-   });
+  // Toggle the "Scroll Up" link visibility based on scroll position
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scrollTop").fadeIn();
+    } else {
+      $("#scrollTop").fadeOut();
+    }
+  });
 
   $("a.nav-link").click(function () {
     // Pill UI:
@@ -44,6 +44,7 @@ $(() => {
   async function handleHome() {
     const coins = await getJson("coins.json");
     displayCoins(coins);
+    
 
     // Clear the selectedCoinIds from local storage
     localStorage.removeItem("selectedCoinIds");
@@ -72,31 +73,12 @@ $(() => {
   });
 
   // Displaying Crypto Coins and adding them to the Page
-    function displayCoins(coins) {
-      
-    coins = coins.filter(c => c.name.length <= 8);
+  function displayCoins(coins) {
+    coins = coins.filter((c) => c.name.length <= 8);
     let html = "";
-    for (let i = 0; i < 100; i++) {
-      html += `
-        <div id="card" class="card">
-          <div id="cardBody" class="card-body">
-            <label class="switch">
-              <input id="slider_${coins[i].id}" type="checkbox">
-              <span id="sliderButton" class="slider round"></span>
-            </label>
-            <h5 class="card-title">${coins[i].symbol}</h5>
-            <p class="card-text">${coins[i].name}</p>
-            <button id="button_${coins[i].id}" class="btn btn-primary more-info" data-bs-toggle="collapse" data-bs-target="#collapse_${coins[i].id}">
-              Coin Info
-            </button>
-            <div id="coinMoreInfo">
-              <div class="collapse collapse-vertical" id="collapse_${coins[i].id}">
-                <div class="card card-body" style="width: 200px;"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
+    
+    for (let i = 0; i < 100; i++) { 
+      html += createCoinCard(coins[i]);
     }
     $("#coinsContainer").html(html);
 
@@ -109,6 +91,29 @@ $(() => {
       }
     });
 
+    function createCoinCard(coin) {
+      return `
+          <div id="card" class="card">
+            <div id="cardBody" class="card-body">
+              <label class="switch">
+                <input id="slider_${coin.id}" type="checkbox">
+                <span id="sliderButton" class="slider round"></span>
+              </label>
+              <h5 class="card-title">${coin.symbol}</h5>
+              <p class="card-text">${coin.name}</p>
+              <button id="button_${coin.id}" class="btn btn-primary more-info" data-bs-toggle="collapse" data-bs-target="#collapse_${coin.id}">
+                Coin Info
+              </button>
+              <div id="coinMoreInfo">
+                <div class="collapse collapse-vertical" id="collapse_${coin.id}">
+                  <div class="card card-body" style="width: 200px;"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+    }
+
     // Creating The Selected Coins By An Array:
     let selectedCoins = [];
 
@@ -120,7 +125,7 @@ $(() => {
       return;
     }
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < coins.length; i++) {
       const checkbox = $(`#slider_${coins[i].id}`);
 
       checkbox.on("change", function () {
@@ -130,8 +135,8 @@ $(() => {
         // When user check coin , it push it to the array:
         if (isChecked) {
           if (selectedCoins.length < maxCoins) {
-            const coin = coins.find(c => c.id === coinId);
-            if (!selectedCoins.some(c => c.id === coinId)) {
+            const coin = coins.find((c) => c.id === coinId);
+            if (!selectedCoins.some((c) => c.id === coinId)) {
               selectedCoins.push(coin);
             }
             // When user check the sixth coin , create an offer for replacing on the modal:
@@ -144,13 +149,13 @@ $(() => {
             checkbox.prop("checked", false);
             $("#maxCoinsModal").modal("show");
 
-            const coin = coins.find(c => c.id === coinId);
-            if (!selectedCoins.some(c => c.id === coinId)) {
+            const coin = coins.find((c) => c.id === coinId);
+            if (!selectedCoins.some((c) => c.id === coinId)) {
               selectedCoins.push(coin);
             }
           }
         } else {
-          selectedCoins = selectedCoins.filter(c => c.id !== coinId);
+          selectedCoins = selectedCoins.filter((c) => c.id !== coinId);
         }
 
         updateSelectedCoinsModal();
@@ -205,7 +210,7 @@ $(() => {
 
           if (!isChecked) {
             // Remove the unselected coin from the array
-            selectedCoins = selectedCoins.filter(coin => coin.id !== coinId);
+            selectedCoins = selectedCoins.filter((coin) => coin.id !== coinId);
 
             $(`#slider_${coinId}`).prop("checked", false);
             updateSelectedCoinsModal();
@@ -219,16 +224,16 @@ $(() => {
       coinsSelected.innerHTML = "";
 
       // Add only the symbols of selected coins to coinsSelected
-      selectedCoins.forEach(coin => {
+      selectedCoins.forEach((coin) => {
         const coinSymbol = coin.symbol;
         const coinSymbolElement = document.createElement("span");
         coinSymbolElement.classList.add("selectedCoins");
-        coinSymbolElement.innerText = coinSymbol + " ";
+        coinSymbolElement.innerText = coinSymbol + " | ";
         coinsSelected.appendChild(coinSymbolElement);
       });
 
       // Create an array of selected coin IDs
-      const selectedCoinIds = selectedCoins.map(coin => coin.symbol);
+      const selectedCoinIds = selectedCoins.map((coin) => coin.symbol);
 
       // Store the selected coin IDs in the local storage
       localStorage.setItem("selectedCoinIds", JSON.stringify(selectedCoinIds));
@@ -253,7 +258,7 @@ $(() => {
   });
 
   let dataIntervalId;
-    let chart;
+  let chart;
 
   // Function to clear the graph
   function clearGraph() {
@@ -261,8 +266,8 @@ $(() => {
       chart.destroy();
       chart = null;
     }
-    }
-    
+  }
+
   $("#reportsLink").on("click", async function () {
     clearInterval(dataIntervalId);
     clearGraph();
@@ -270,202 +275,200 @@ $(() => {
 
     const selectedCoinIds = JSON.parse(localStorage.getItem("selectedCoinIds"));
 
-   if (!selectedCoinIds || selectedCoinIds.length !== 5) {
-     $("#chartMessage").html(
-       `<div class="noData"> <h2>Please select 5 coins to display on the graph!</h2> </div>`
-     );
-   } else {
-       $("#chartMessage").html(
-         `<div class="noData"> 
+    if (!selectedCoinIds) {
+      $("#chartMessage").html(
+        `<div class="noData"> <h2>Please select coins to display on the graph!</h2> </div>`
+      );
+    } else {
+      $("#chartMessage").html(
+        `<div class="noData"> 
            <h2>Loading...</h2>
              </div>`
-       );
-     let coinOne = [];
-     let coinTwo = [];
-     let coinThree = [];
-     let coinFour = [];
-     let coinFive = [];
-     let coinName = [];
+      );
+      let coinOne = [];
+      let coinTwo = [];
+      let coinThree = [];
+      let coinFour = [];
+      let coinFive = [];
+      let coinName = [];
 
-     function getData(event) {
-       const coinsSelected = selectedCoinIds.slice(0, 5);
-       $.ajax({
-         type: "GET",
-         url: `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinsSelected.join(
-           ","
-         )}&tsyms=USD`,
+      function getData(event) {
+        const coinsSelected = selectedCoinIds.slice(0, 5);
+        $.ajax({
+          type: "GET",
+          url: `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinsSelected.join(
+            ","
+          )}&tsyms=USD`,
 
-         success: function (result) {
-           if (result.Response === "Error") {
-             clearInterval(dataIntervalId);
-             $("#chartMessage").html(
-               `<div class="noData"> <h2>No data on selected currencies - please try other coins!</h2> </div>`
-             );
-           } else {
-             $("#chartMessage").html(` <div id="chartContainer""></div>`);
+          success: function (result) {
+            if (result.Response === "Error") {
+              clearInterval(dataIntervalId);
+              $("#chartMessage").html(
+                `<div class="noData"> <h2>No data on selected currencies - please try other coins!</h2> </div>`
+              );
+            } else {
+              $("#chartMessage").html(` <div id="chartContainer""></div>`);
 
-             let dateNow = new Date();
-             coinName = [];
+              let dateNow = new Date();
+              coinName = [];
 
-             Object.entries(result).forEach(([key, value], index) => {
-               const coinData = {
-                 x: dateNow,
-                 y: value.USD,
-               };
+              Object.entries(result).forEach(([key, value], index) => {
+                const coinData = {
+                  x: dateNow,
+                  y: value.USD,
+                };
 
-               // Push the coin real-time data to the corresponding array
-               switch (index) {
-                 case 0:
-                   coinOne.push(coinData);
-                   break;
-                 case 1:
-                   coinTwo.push(coinData);
-                   break;
-                 case 2:
-                   coinThree.push(coinData);
-                   break;
-                 case 3:
-                   coinFour.push(coinData);
-                   break;
-                 case 4:
-                   coinFive.push(coinData);
-                   break;
-                 default:
-                   break;
-               }
+                // Push the coin real-time data to the corresponding array
+                switch (index) {
+                  case 0:
+                    coinOne.push(coinData);
+                    break;
+                  case 1:
+                    coinTwo.push(coinData);
+                    break;
+                  case 2:
+                    coinThree.push(coinData);
+                    break;
+                  case 3:
+                    coinFour.push(coinData);
+                    break;
+                  case 4:
+                    coinFive.push(coinData);
+                    break;
+                  default:
+                    break;
+                }
 
-               // Push the coin name to the array of real-time names
-               coinName.push(key);
-             });
+                // Push the coin name to the array of real-time names
+                coinName.push(key);
+              });
 
-             createGraph();
-           }
-         },
-       });
-     }
+              createGraph();
+            }
+          },
+        });
+      }
 
-     dataIntervalId = setInterval(() => {
-       getData();
-     }, 2000);
+      dataIntervalId = setInterval(() => {
+        getData();
+      }, 2000);
 
-     function createGraph(event) {
-       let chart = new CanvasJS.Chart("chartContainer", {
-         exportEnabled: true,
-         animationEnabled: false,
-         theme: "light2",
+      function createGraph(event) {
+        let chart = new CanvasJS.Chart("chartContainer", {
+          exportEnabled: true,
+          animationEnabled: false,
+          theme: "light2",
 
-         title: {
-           text: "Real-time Price of Selected CryptoCurrencies in $USD",
-         },
-         axisX: {
-           valueFormatString: "HH:mm:ss",
-         },
-         axisY: {
-           title: "Coin Value",
-           suffix: "$",
-           titleFontColor: "#4F81BC",
-           lineColor: "#00A0FF",
-           labelFontColor: "#4F81BC",
-           tickColor: "#4F81BC",
-           includeZero: true,
-         },
-         toolTip: {
-           shared: true,
-         },
-         legend: {
-           cursor: "pointer",
-           itemclick: toggleDataSeries,
-         },
-         data: [
-           {
-             type: "stepLine",
-             name: coinName[0],
-             showInLegend: true,
-             xValueFormatString: "HH:mm:ss",
-             dataPoints: coinOne,
-             lineThickness: 5,
-             color: "#4F81BC",
-           },
-           {
-             type: "stepLine",
-             name: coinName[1],
-             showInLegend: true,
-             xValueFormatString: "HH:mm:ss",
-             dataPoints: coinTwo,
-             lineThickness: 5,
-             color: "#C0504E",
-           },
-           {
-             type: "stepLine",
-             name: coinName[2],
-             showInLegend: true,
-             xValueFormatString: "HH:mm:ss",
-             dataPoints: coinThree,
-             lineThickness: 5,
-             color: "#9BBB58",
-           },
-           {
-             type: "stepLine",
-             name: coinName[3],
-             showInLegend: true,
-             xValueFormatString: "HH:mm:ss",
-             dataPoints: coinFour,
-             lineThickness: 5,
-             color: "#604A7B",
-           },
-           {
-             type: "stepLine",
-             name: coinName[4],
-             showInLegend: true,
-             xValueFormatString: "HH:mm:ss",
-             dataPoints: coinFive,
-             lineThickness: 5,
-             color: "#F08000",
-           },
-         ],
-       });
+          title: {
+            text: "Real-time Price of Selected CryptoCurrencies in $USD",
+          },
+          axisX: {
+            valueFormatString: "HH:mm:ss",
+          },
+          axisY: {
+            title: "Coin Value",
+            suffix: "$",
+            titleFontColor: "#4F81BC",
+            lineColor: "#00A0FF",
+            labelFontColor: "#4F81BC",
+            tickColor: "#4F81BC",
+            includeZero: true,
+          },
+          toolTip: {
+            shared: true,
+          },
+          legend: {
+            cursor: "pointer",
+            itemclick: toggleDataSeries,
+          },
+          data: [
+            {
+              type: "stepLine",
+              name: coinName[0],
+              showInLegend: true,
+              xValueFormatString: "HH:mm:ss",
+              dataPoints: coinOne,
+              lineThickness: 5,
+              color: "#4F81BC",
+            },
+            {
+              type: "stepLine",
+              name: coinName[1],
+              showInLegend: true,
+              xValueFormatString: "HH:mm:ss",
+              dataPoints: coinTwo,
+              lineThickness: 5,
+              color: "#C0504E",
+            },
+            {
+              type: "stepLine",
+              name: coinName[2],
+              showInLegend: true,
+              xValueFormatString: "HH:mm:ss",
+              dataPoints: coinThree,
+              lineThickness: 5,
+              color: "#9BBB58",
+            },
+            {
+              type: "stepLine",
+              name: coinName[3],
+              showInLegend: true,
+              xValueFormatString: "HH:mm:ss",
+              dataPoints: coinFour,
+              lineThickness: 5,
+              color: "#604A7B",
+            },
+            {
+              type: "stepLine",
+              name: coinName[4],
+              showInLegend: true,
+              xValueFormatString: "HH:mm:ss",
+              dataPoints: coinFive,
+              lineThickness: 5,
+              color: "#F08000",
+            },
+          ],
+        });
 
-       chart.render();
+        chart.render();
 
-       function toggleDataSeries(e) {
-         if (
-           typeof e.dataSeries.visible === "undefined" ||
-           e.dataSeries.visible
-         ) {
-           e.dataSeries.visible = false;
-         } else {
-           e.dataSeries.visible = true;
-         }
-       }
-     }
-   }
+        function toggleDataSeries(e) {
+          if (
+            typeof e.dataSeries.visible === "undefined" ||
+            e.dataSeries.visible
+          ) {
+            e.dataSeries.visible = false;
+          } else {
+            e.dataSeries.visible = true;
+          }
+        }
+      }
+    }
   });
 
   // Take data for more info on every coin:
-
   async function handleMoreInfo(coinId) {
     const localStorageKey = "coin_" + coinId;
     const storedData = localStorage.getItem(localStorageKey);
 
-    // Check if there is stored data already , every 2 minutes cooldown for the API:
+    // Check if there is stored data already and if it's not expired
     if (storedData) {
       const data = JSON.parse(storedData);
       const currentTime = new Date().getTime();
-      if (currentTime - data.timestamp < 120000 /*2 Minutes*/) {
+      if (currentTime - data.timestamp < 120000 /* 2 Minutes */) {
         const { imageSource, usd, eur, ils } = data;
         const moreInfo = `
-        <img src="${imageSource}"><br>
-        USD: $${usd} <br>
-        USD: €${eur} <br>
-        USD: ₪${ils} <br>
-      `;
+          <img src="${imageSource}"><br>
+          USD: $${usd} <br>
+          USD: €${eur} <br>
+          USD: ₪${ils} <br>
+        `;
         $(`#collapse_${coinId}`).html(moreInfo);
         return;
       }
     }
 
     // API for the coins info:
-
     const coin = await getJson(
       "https://api.coingecko.com/api/v3/coins/" + coinId
     );
@@ -474,14 +477,14 @@ $(() => {
     const eur = coin.market_data.current_price.eur;
     const ils = coin.market_data.current_price.ils;
     const moreInfo = `
-    <img src="${imageSource}"><br>
-    USD: $${usd} <br>
-    USD: €${eur} <br>
-    USD: ₪${ils} <br>
-  `;
+      <img src="${imageSource}"><br>
+      USD: $${usd} <br>
+      USD: €${eur} <br>
+      USD: ₪${ils} <br>
+    `;
     $(`#collapse_${coinId}`).html(moreInfo);
 
-    // Real time date :
+    // Store the updated data in local storage
     const newData = {
       imageSource,
       usd,
@@ -491,7 +494,8 @@ $(() => {
     };
     localStorage.setItem(localStorageKey, JSON.stringify(newData));
   }
-  const getJson = async path => {
+
+  const getJson = async (path) => {
     return $.ajax({
       url: path,
       dataType: "json",
